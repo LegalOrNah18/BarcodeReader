ad82773bd9991220bf429fe54a6be257d2536fad
Kavin Jinasena <kavinj619@gmail.com>
2019-11-13 17:49:05 +0000

    Made a method that adds into the database. Most of the things for this Sprint is to make more methods for different queries.

--- BarcodeScanner/src/Database.java
+++ BarcodeScanner/src/Database.java
@@ -1,27 +1,52 @@
 import java.sql.*;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
+
 public class Database {
-	public static void myConnection() {
-		
+	
+	public static Connection connect() {
+		Connection conn=null;
+		try {
+			conn=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/students","root","Legal69!");
+			
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		return conn;
 	}
 	
-	public static void main(String[] args) {
-		try {
-			//Connecting to the database;
-			Class.forName("com.mysql.cj.jdbc.Driver");  
-			Connection conn=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/students","root","Legal69!");
-			//Creating a statement query for mySQL
-			Statement statement=conn.createStatement();
-			//Executing the query
-			ResultSet result=statement.executeQuery("select * from testing");
-			//getting all values
-			while (result.next()) {
-				System.out.println(result.getString("First_Name")+ ", "+result.getString("Last_Name")+", "+result.getInt("ID"));
-			}
-			
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	public void add(String fname, String lname, int id) {
+		String sql="INSERT INTO TESTING(First_Name, Last_Name,ID)VALUES(?,?,?)";
+		try (Connection conn=this.connect()){
+			PreparedStatement statement=conn.prepareStatement(sql);
+			statement.setString(1, fname);
+			statement.setString(2, lname);
+			statement.setInt(3, id);
+			statement.executeUpdate();
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
 		}
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		Database data=new Database();
+		data.add("DYLAN","PALK",18092923);
+		try {
+			Connection myconn=connect();
+			Statement stmt=myconn.createStatement();
+			String query="SELECT * FROM testing";
+			ResultSet result=stmt.executeQuery(query);
+			while (result.next()) {
+				System.out.println(result.getInt(1)+" "+result.getString(2)+" "+result.getString(3));
+			}
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		
+		
+		
 	}
 }
