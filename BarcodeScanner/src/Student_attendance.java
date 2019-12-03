19abe90ca208113ce1600e331ffa5cbe41d23d3e
AmeerJ <AmeerJ@10.24.1.118>
2019-12-03 12:08:36 +0000

    created student attendance

---
+++ BarcodeScanner/src/Student_attendance.java
@@ -0,0 +1,22 @@
+
+public class Student_attendance {
+	
+	public String name;
+	public int id;
+	public String last_name;
+	
+	
+	public int getID() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getLName() {
+		return last_name;
+	}
+	
+	
+}
