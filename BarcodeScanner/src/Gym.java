3523715e582d408d0201a8324af4296db26b0e11
Kavin Jinasena <kavinj619@gmail.com>
2019-12-04 22:54:36 +0000

    Gym Class updated

--- BarcodeScanner/src/Gym.java
+++ BarcodeScanner/src/Gym.java
@@ -1,4 +1,20 @@
 
 public class Gym {
+	
+	public String name;
+	public int id;
+	public String membershipType;
+	
+	public String getType() {
+		return membershipType;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getID() {
+		return id;
+	}
 
 }
