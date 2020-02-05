8773a3512e5155ada849bbef9c86e1f95c770d1c
Kavin Jinasena <kavinj619@gmail.com>
2020-02-05 01:32:54 +0000

    update to main method

--- BarcodeScanner/src/Database.java
+++ BarcodeScanner/src/Database.java
@@ -32,6 +32,7 @@ public class Database {
 		}
 	}
 	
+	
 	public void remove(int id) {
 		String sql="DELETE FROM student WHERE id=?";
 		try (Connection conn=this.connect()){
@@ -62,21 +63,91 @@ public class Database {
 	
 	
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
 		Database data=new Database();
 		Scanner scan=new Scanner(System.in);
-		data.add("KEVAN", "JINASENA", 18058289, "JAPANESE STUDIES AND COMPSCI");
-		try {
-			Connection myconn=connect();
-			Statement stmt=myconn.createStatement();
-			String query="SELECT * FROM student";
-			ResultSet result=stmt.executeQuery(query);
-			while (result.next()) {
-				System.out.println(result.getInt(1)+" "+result.getString(2)+" "+result.getString(3)+" "+result.getString(4));
+		
+		System.out.println("Student or Teacher");
+		
+		String choice=scan.next();
+		
+		if (choice.equals("s")) {
+			try {
+				Connection myconn=connect();
+				System.out.println("Decide Action: Add, Delete, Update or View");
+				String choice1= scan.next();
+				if (choice1.equals("view")) {
+					System.out.println("Enter student ID");
+					int studID=scan.nextInt();
+					
+					String query="SELECT * FROM student WHERE ID="+studID;
+					PreparedStatement ps=myconn.prepareStatement(query);
+					
+					ResultSet result=ps.executeQuery(query);
+					try {
+						while (result.next()) {
+							System.out.println(result.getInt(1)+" "+result.getString(2)+" "+result.getString(3)+" "+result.getString(4));
+						}
+					}catch (Exception e) {
+						System.out.println(e);
+					}
+					
+					
+				}
+				
+				else if (choice1.equals("add")) {
+					System.out.println("ID of new student: ");
+					int id=scan.nextInt();
+					System.out.println("First name of new student: ");
+					String fname=scan.next();
+					System.out.println("Last Name of new student: ");
+					String lname=scan.next();
+					System.out.println("Course of new student: ");
+					String course=scan.next();
+					
+					String query="INSERT INTO student(First_Name, Last_Name,ID, Course)VALUES(?,?,?,?)";
+					PreparedStatement statement=myconn.prepareStatement(query);
+					statement.setString(1, fname);
+					statement.setString(2, lname);
+					statement.setInt(3, id);
+					statement.setString(4, course);
+					statement.executeUpdate();
+					
+				}
+				
+				else if (choice1.equals("delete")) {
+					System.out.println("Enter ID of student to remove: ");
+					int id=scan.nextInt();
+					String sql="DELETE FROM student WHERE id=?";
+					PreparedStatement statement=myconn.prepareStatement(sql);
+					statement.setInt(1, id);
+					statement.executeUpdate();
+				}
+				
+				else if (choice1.equals("update")) {
+					String sql="UPDATE student SET First_Name = ?, Last_Name = ?, Course=? WHERE id = ?";
+					System.out.println("Enter ID to update: ");
+					int id=scan.nextInt();
+					System.out.println("Enter First name to update: ");
+					String fname=scan.next();
+					System.out.println("Enter Last Name to update: ");
+					String lname=scan.next();
+					System.out.println("Enter Course to update: ");
+					String course=scan.next();
+					PreparedStatement ps=myconn.prepareStatement(sql);
+					ps.setString(1, fname);
+					ps.setString(2, lname);
+					ps.setString(3, course);
+					ps.setInt(4, id);
+					ps.executeUpdate();
+					ps.close();
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-		}catch(Exception e) {
-			System.out.println(e.getMessage());
 		}
+		
 		
 		
 		
